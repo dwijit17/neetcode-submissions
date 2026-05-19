43531ed91c979fa497275398b68998174f989d05
Dwijit <105113944+dwijit17@users.noreply.github.com>
2026-05-19 18:34:21 +0530

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        stack = []
+        hmap = {}
+        n = len(speed)
+        for i in range(n):
+            hmap[position[i]] = speed[i]
+        
+        #sort the hmap by order of kyes
+        dis = sorted(hmap,key = lambda x : x,reverse=True)
+        #we got distances sorted in descending order
+        #now calculate time
+        time = [(target-d)/hmap[d] for d in dis]
+        for i in range(n):
+            if len(stack)>0:
+                if time[i]<=stack[-1]:
+                    continue
+            stack.append(time[i])
+        return len(stack)
+        
+        
