75d4fd204a25a11b4c1d6b74c91cebe52dae87f1
Dwijit <105113944+dwijit17@users.noreply.github.com>
2026-08-01 12:33:14 +0530

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        hmap = {}
+        def helper(s):
+            if s=="":
+                return 1
+            if s in hmap:
+                return hmap[s]
+            onedigit = twodigit = 0
+            if 0<int(s[:1])<=9:
+                onedigit = helper(s[1:])
+            if len(s)>=2 and 10<=int(s[:2])<=26:
+                twodigit = helper(s[2:])
+            hmap[s] = onedigit+twodigit
+            return hmap[s]
+        return helper(s)
